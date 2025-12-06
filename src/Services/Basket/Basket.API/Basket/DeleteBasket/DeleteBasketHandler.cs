1290152bbabca6d6e95eda48e6fb8d22c9702328
iliasKout87 <ilias.koutlas@betssongroup.com>
2025-12-06 13:53:18 +0100

    Basket API

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,26 @@
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required");
+    }
+}
+
+
+internal class DeleteBasketCommandHandler(IBasketRepository repository) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        //TODO: Delete basket from database
+        //TODO: Delete basket from cache
+        var result = await repository.DeleteBasketAsync(command.UserName, cancellationToken);
+
+        return new DeleteBasketResult(result);
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,25 @@
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart is required");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("User name is required");
+    }
+}
+
+internal class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        //TODO: update cache
+        var basket = await repository.StoreBasketAsync(command.Cart, cancellationToken);
+
+        return new StoreBasketResult(basket.UserName);
+    }
+}
